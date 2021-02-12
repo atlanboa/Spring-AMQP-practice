7b8c6dd64faf57298ed6cf766539a87f7126c451
sangheon kim <atlanboa@gmail.com>
2021-02-12 21:54:01 +0900

    receiver sender sample in rabbitmq doc

--- src/main/java/com/akmj/rabbitmq/RabbitmqApplication.java
+++ src/main/java/com/akmj/rabbitmq/RabbitmqApplication.java
@@ -7,49 +7,33 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
 import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
 import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class RabbitmqApplication {
 
-	static final String topicExchangeName = "spring-boot-exchange";
-
-	static final String queueName = "spring-boot";
-
+	@Profile("usage_message")
 	@Bean
-	Queue queue() {
-		return new Queue(queueName, false);
+	public CommandLineRunner usage() {
+		return args -> {
+			System.out.println("This app uses Spring Profiles to control its behavior.\n");
+			System.out.println("Sample usage: java -jar rabbit-tutorials.jar --spring.profiles.active=hello-world,sender");
+		};
 	}
 
+	@Profile("!usage_message")
 	@Bean
-	TopicExchange exchange() {
-		return new TopicExchange(topicExchangeName);
+	public CommandLineRunner tutorial() {
+		return new Runner();
 	}
 
-	@Bean
-	Binding binding(Queue queue, TopicExchange exchange) {
-		return BindingBuilder.bind(queue).to(exchange).with("foo.bar.#");
+	public static void main(String[] args) throws Exception {
+		SpringApplication.run(RabbitmqApplication.class, args);
 	}
-
-	@Bean
-	SimpleMessageListenerContainer container(ConnectionFactory connectionFactory,
-											 MessageListenerAdapter listenerAdapter) {
-		SimpleMessageListenerContainer container = new SimpleMessageListenerContainer();
-		container.setConnectionFactory(connectionFactory);
-		container.setQueueNames(queueName);
-		container.setMessageListener(listenerAdapter);
-		return container;
-	}
-
-	@Bean
-	MessageListenerAdapter listenerAdapter(Receiver receiver) {
-		return new MessageListenerAdapter(receiver, "receiveMessage");
-	}
-
-	public static void main(String[] args) throws InterruptedException {
-		SpringApplication.run(RabbitmqApplication.class, args).close();
-	}
-
 }
--- src/main/java/com/akmj/rabbitmq/Runner.java
+++ src/main/java/com/akmj/rabbitmq/Runner.java
@@ -1,27 +1,22 @@
 package com.akmj.rabbitmq;
 
-import java.util.concurrent.TimeUnit;
-
-import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
+import org.springframework.context.ConfigurableApplicationContext;
 
-@Component
 public class Runner implements CommandLineRunner {
 
-    private final RabbitTemplate rabbitTemplate;
-    private final Receiver receiver;
+    @Value("${tutorial.client.duration:0}")
+    private int duration;
 
-    public Runner(Receiver receiver, RabbitTemplate rabbitTemplate) {
-        this.receiver = receiver;
-        this.rabbitTemplate = rabbitTemplate;
-    }
+    @Autowired
+    private ConfigurableApplicationContext ctx;
 
     @Override
-    public void run(String... args) throws Exception {
-        System.out.println("Sending message...");
-        rabbitTemplate.convertAndSend(RabbitmqApplication.topicExchangeName, "foo.bar.baz", "Hello from RabbitMQ!");
-        receiver.getLatch().await(10000, TimeUnit.MILLISECONDS);
+    public void run(String... arg0) throws Exception {
+        System.out.println("Ready ... running for " + duration + "ms");
+        Thread.sleep(duration);
+        ctx.close();
     }
-
 }
